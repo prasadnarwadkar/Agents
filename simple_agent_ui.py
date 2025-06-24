cdb3227cd3aa690c2cb6a3f55a3b8ed22b23e482
Prasad Narwadkar <prasad.narwadkar@live.in>
2025-06-24 23:17:24 +0530

    Open AI API key and Vector store id must be now entered.

--- simple_agent_ui.py
+++ simple_agent_ui.py
@@ -1,13 +1,9 @@
-import os
 from openai import OpenAI
-import mysettings
 
-client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
-
-vector_store_id = mysettings.MySettings.get_static()
-vector_store = client.vector_stores.retrieve(
-    vector_store_id=vector_store_id
-)
+openai_api_key = ""
+vector_store_id = ""
+client:OpenAI = None
+vector_store = None
 
 try:
     import gradio as gr
@@ -16,7 +12,30 @@ except ImportError as e:
         "Please install gradio with `pip install gradio`. You must use python>=3.10."
     ) from e
 
-def chat_with_openai(input_text:str,  chatbot: list[dict], message_history: list):
+
+def vector_store_id_text_change(vector_store_id_text: str):
+    global vector_store
+    vector_store_id = vector_store_id_text
+
+    try:
+
+        if (len(vector_store_id) > 0):
+            vector_store = client.vector_stores.retrieve(vector_store_id=vector_store_id)
+    except Exception as e:
+        print(f"An error occurred: {e}")
+
+def openai_api_key_text_change(openai_api_key_text: str):
+    openai_api_key = openai_api_key_text
+    global client
+
+    try:
+
+        if (len(openai_api_key_text) > 0):
+            client  = OpenAI(api_key=openai_api_key)     
+    except Exception as e:
+        print(f"An error occurred: {e}")
+
+def chat_with_openai(input_text: str, chatbot: list[dict], message_history: list):
     # Append user message to history
     message_history.append({"role": "user", "content": input_text})
     chatbot.append({"role": "user", "content": input_text})
@@ -33,8 +52,8 @@ def chat_with_openai(input_text:str,  chatbot: list[dict], message_history: list
         ],
     )
 
-    chatbot.append({'role': 'assistant', 'content': ''})
-    
+    chatbot.append({"role": "assistant", "content": ""})
+
     response_text = ""
     for event in response_stream:
         if event.type == "response.output_text.delta":
@@ -44,36 +63,50 @@ def chat_with_openai(input_text:str,  chatbot: list[dict], message_history: list
             print(f"\nError occurred: {event.error}")
         # Append assistant response to history
         message_history.append({"role": "assistant", "content": response_text})
-        chatbot[-1]['content'] = response_text
-            
+        chatbot[-1]["content"] = response_text
+
         yield gr.skip(), chatbot, gr.skip()
 
     yield gr.Textbox(interactive=True), chatbot, message_history
 
+
 def select_data(message: gr.SelectData) -> str:
-    return message.value['text']
+    return message.value["text"]
 
 
 with gr.Blocks() as demo:
     # Maintain message history
     message_history = gr.State([])
     chatbot = gr.Chatbot(
-        label='Your Friendly Assistant',
-        type='messages',
-        
-        avatar_images=(None, 'https://ai.pydantic.dev/img/logo-white.svg'),
+        label="Your Friendly Assistant",
+        type="messages",
+        avatar_images=(None, "https://ai.pydantic.dev/img/logo-white.svg"),
         examples=[
-            {'text': 'What is Masre Dox?', 'alt_text': ''},
+            {"text": "What is Masre Dox?", "alt_text": ""},
         ],
     )
     with gr.Row():
         with gr.Column():
-            gr.DeepLinkButton(interactive=True,  link='https://skolo-ai-agent.ams3.cdn.digitaloceanspaces.com/pydantic/the_seven_realms.pdf')
+            gr.DeepLinkButton(
+                interactive=True,
+                link="https://skolo-ai-agent.ams3.cdn.digitaloceanspaces.com/pydantic/the_seven_realms.pdf",
+            )
+            openai_api_key_text = gr.Textbox(label="Please enter your OPENAI_API_KEY")
+            vector_store_id_text = gr.Textbox(
+                label="Please create a Vector store on OpenAI. Add the above PDF to that store and enter the vector store ID here."
+            )
     with gr.Row():
         with gr.Column():
             user_input = gr.Textbox(label="Ask Questions Related to the above PDF")
 
-    user_input.submit(fn=chat_with_openai, inputs=[user_input,chatbot,message_history], outputs=[user_input,chatbot,message_history])
+    vector_store_id_text.change(fn=vector_store_id_text_change, inputs=[vector_store_id_text], outputs=[])
+    openai_api_key_text.change(fn=openai_api_key_text_change,  inputs=[openai_api_key_text], outputs=[])
+
+    user_input.submit(
+        fn=chat_with_openai,
+        inputs=[user_input, chatbot, message_history],
+        outputs=[user_input, chatbot, message_history],
+    )
     chatbot.example_select(select_data, None, [user_input])
     demo.queue()
     demo.launch()
